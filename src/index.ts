ff0332218da333afd81db8a5ad16661f47ba212c
dotslashf <fadhlurahman.i@gmail.com>
2021-07-10 10:08:02 +0800

    Add sleep

--- src/index.ts
+++ src/index.ts
@@ -3,36 +3,40 @@ import { Client } from './wrapper/anilist';
 import { randomNumber, randomType } from './utils/randomizer';
 import { Firebase } from './wrapper/firebase';
 import { downloadImage, joinImage } from './utils/imageManipulator';
+import { sleep } from './utils/sleep';
 
 const apolloClient = new Client();
 const firebase = new Firebase();
+const bot = new TwitterClient();
 
 (async () => {
-  const bot = new TwitterClient();
-  const type = randomType();
-  const { n, nPage } = randomNumber(type);
+  while (true) {
+    const type = randomType();
+    const { n, nPage } = randomNumber(type);
 
-  if (type === 'anime') {
-    const anime = await apolloClient.getRandomAnime(nPage, n);
-    const isAnimeExist = await firebase.isAnimeExist(anime);
-    if (!isAnimeExist) {
-      const buffer = await downloadImage(anime.coverImage.extraLarge);
-      await joinImage(buffer);
-      await bot.tweet(
-        `${anime.title.userPreferred} (${anime.title.native}/${anime.title.romaji}) jelek`
-      );
-      await firebase.addAnime(anime);
+    if (type === 'anime') {
+      const anime = await apolloClient.getRandomAnime(nPage, n);
+      const isAnimeExist = await firebase.isAnimeExist(anime);
+      if (!isAnimeExist) {
+        const buffer = await downloadImage(anime.coverImage.extraLarge);
+        await joinImage(buffer);
+        await bot.tweet(
+          `${anime.title.userPreferred} (${anime.title.native}/${anime.title.romaji}) jelek`
+        );
+        await firebase.addAnime(anime);
+      }
+    } else if (type === 'character') {
+      const character = await apolloClient.getRandomCharacter(nPage, n);
+      const isCharacterExist = await firebase.isCharacterExist(character);
+      if (!isCharacterExist) {
+        const buffer = await downloadImage(character.image.large);
+        await joinImage(buffer);
+        await bot.tweet(
+          `${character.name.userPreferred} (${character.name.native}) jelek`
+        );
+        await firebase.addCharacter(character);
+      }
     }
-  } else if (type === 'character') {
-    const character = await apolloClient.getRandomCharacter(nPage, n);
-    const isCharacterExist = await firebase.isCharacterExist(character);
-    if (!isCharacterExist) {
-      const buffer = await downloadImage(character.image.large);
-      await joinImage(buffer);
-      await bot.tweet(
-        `${character.name.userPreferred} (${character.name.native}) jelek`
-      );
-      await firebase.addCharacter(character);
-    }
+    await sleep(30);
   }
 })();
